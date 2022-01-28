cecee6c06a8b707565cbbd6a1d03335e4ae7ba8a
Konstantin204 <konstan.georgiev@gmail.com>
2022-01-28 13:29:58 +0200

    Add project files.

---
+++ QuickSortWithPanels/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace QuickSortWithPanels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(619, 194);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(41, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel2.Location = new System.Drawing.Point(524, 194);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(41, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel3.Location = new System.Drawing.Point(425, 194);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(41, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel4.Location = new System.Drawing.Point(335, 194);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(41, 100);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel5.Location = new System.Drawing.Point(243, 194);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(41, 100);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel6.Location = new System.Drawing.Point(147, 194);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(41, 100);
+            this.panel6.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(533, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(673, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(20, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(760, 73);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Quick Sort Visualizer";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ QuickSortWithPanels/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace QuickSortWithPanels
+{
+    public partial class Form1 : Form
+    {
+        int[] Array = new int[6];
+        List<Panel> panels = new List<Panel>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private int Partition(int[] array, int low, int high)
+        {
+           
+            int pivot = array[high];
+            int lowIndex = (low - 1);
+
+            
+            for (int j = low; j < high; j++)
+            {
+                printCharts(array, 250, j, -1);
+
+                if (array[j] <= pivot)
+                {
+                    lowIndex++;
+
+                    Swap(ref array, lowIndex, j);
+                }
+            }
+
+            Swap(ref array, lowIndex + 1, high);
+
+            return lowIndex + 1;
+        }
+
+        private void Swap(ref int[] array, int index1, int index2)
+        {
+            printCharts(array, 350, index1, index2);
+            int temp1 = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp1;
+            printCharts(array, 350, index1, index2);
+        }
+
+        private void Sort(int[] array, int low, int high)
+        {
+            Application.DoEvents();
+            if (low < high)
+            {
+                int partitionIndex = Partition(array, low, high);
+
+                
+
+                printCharts(array, 100);
+                Sort(array, low, partitionIndex - 1);
+
+                printCharts(array, 100);
+                Sort(array, partitionIndex + 1, high);
+            }
+        }
+        private void printCharts(int[] array, int sleep, int lowred = -1, int highred = -1)
+        {
+
+            panel6.Height = array[0] * 20;
+            panel5.Height = array[1] * 20;
+            panel4.Height = array[2] * 20;
+            panel3.Height = array[3] * 20;
+            panel2.Height = array[4] * 20;
+            panel1.Height = array[5] * 20;
+
+            panels.Add(panel6);
+            panels.Add(panel5);
+            panels.Add(panel4);
+            panels.Add(panel3);
+            panels.Add(panel2);
+            panels.Add(panel1);
+
+            foreach (var panel in panels)
+            {
+                panel.BackColor = Color.FromArgb(128, 128, 255);
+            }
+
+            if (lowred >= 0) panels[lowred].BackColor = Color.Red;
+            if (highred >= 0) panels[highred].BackColor = Color.Red;
+
+            Application.DoEvents();
+
+            Thread.Sleep(sleep);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random number = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                Array[i] = number.Next(1, 9);
+            }
+            panel1.Height = Array[5] * 20;
+            panel2.Height = Array[4] * 20;
+            panel3.Height = Array[3] * 20;
+            panel4.Height = Array[2] * 20;
+            panel5.Height = Array[1] * 20;
+            panel6.Height = Array[0] * 20;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Sort(Array, 0, Array.Length - 1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
